af1d7185af7d91ec655e2c3510b60cba15fb7d8a
Ahmed Wako <ahmedwako@Ahmeds-MacBook-Air.local>
2021-09-22 08:05:53 -0500

    wtrying to make the form collect user data

--- scripts/client.js
+++ scripts/client.js
@@ -2,5 +2,45 @@ $(document).ready(onReady)
 
 
 function onReady(){
-    console.log('JQ sourced!');
+    console.log('JQ Sourced!');
+
+$(document).on('click', '#submitButton', onSubmit);
+}
+
+
+function onSubmit(){
+
+    let employee = {
+        firstName: $('#firstNameInput').val(),
+        lastName: $('#lastNameInput').val(),
+        id: $('#idInput').val(),
+        title: $('#titleInput').val(),
+        annualSalary: Number ($('#annualSalaryInput').val())
+    };
+    console.log('new employee', employee);
+
+
+    $('#employeeTable').append(`
+        <tr>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.id}</td>
+            <td>${employee.title}</td>
+            <td>$${employee.annualSalary}</td>
+            <td>
+                <button class="deleteEmployee">Delete</button>
+            </td>
+        </tr>
+
+
+    `)
+
+
+    let monthlyCost = employee.annualSalary / 12;
+    $('#totalMonthly').text(monthlyCost.toFixed(2));
+
+
+
+
+
 }
